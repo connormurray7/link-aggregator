f85ebe55137378ad23df52e984b4aaf360f64c42
Connor Murray <connormurray7@gmail.com>
2016-10-12 22:46:11 -0500

    Main method is working

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ def handle_get():
 
 @app.route('/search', methods=['POST'])
 def handle_request():
-    data = request.json['term'] # The search term
+    a = APIParser()
+    term = request.json['term'] # The search term
     app.logger.info("Received: " + data)
-    return "Received the request"
+    return a.request(term)
 
 if __name__ == "__main__":
     handler = RotatingFileHandler('log/link-agg.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     app.logger.setLevel(logging.INFO)
-    #app.run()
-    a = APIParser()
-    a.check_interfaces()
+    app.run()
