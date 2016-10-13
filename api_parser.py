139df35aa3b629a3517b6a7c57437c04032d7511
Connor Murray <connormurray7@gmail.com>
2016-10-12 22:45:35 -0500

    Working parser and hacker news interface

--- api_parser.py
+++ api_parser.py
@@ -11,17 +11,14 @@ class APIParser:
         if(req in self.cache):
             return self.cache[req]
         self.cache[req] = self._request(req)
+        return self.cache[req]
 
     def _request(self, req):
-        r = "["
+        responses = []
         for i in self.interfaces:
-            r += i.get_json(req) + ","
-        return r[:-1]
+            responses.append(i.get_messages(req))
+        return json.dumps(responses, default=LinkAggMessage.to_dict)
 
     def _set_interfaces(self):
         self.interfaces.append(StackOverFlow())
         self.interfaces.append(HackerNews())
-
-    def check_interfaces(self):
-        for i in self.interfaces:
-            print(i.get_json("microservices"))
--- web_interfaces.py
+++ web_interfaces.py
@@ -1,12 +1,23 @@
 #from abc import ABC, abstractmethod
 import requests
+import json
+
+class LinkAggMessage():
+
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
+    def to_dict(o):
+        return o.__dict__
+
 
 class WebInterface:
 
     def __init__(self, url):
         self.base_url = url
 
-    def get_json(self, request):
+    def get_messages(self, request):
         #Make request
         #Parse response to common format
         #Send
@@ -19,9 +30,9 @@ class StackOverFlow(WebInterface):
     def __init__(self):
         super().__init__(self.STACKOVERFLOW_URL)
 
-    def get_json(self, request):
+    def get_messages(self, request):
         print("Inheriting from " + self.base_url)
-        return "StackOverFlow"
+        return {"Stack OverFlow" : []}
 
 class HackerNews(WebInterface):
 
@@ -30,9 +41,10 @@ class HackerNews(WebInterface):
     def __init__(self):
         super().__init__(self.HACKER_NEWS_URL)
 
-    def get_json(self, query):
+    def get_messages(self, query):
+        messages = []
         params = {'query' : query, 'tags' : 'story'}
         response = requests.get(self.base_url, params).json()
         for hit in response['hits']:
-            print(hit['url'])
-        return "hello"
+            messages.append(LinkAggMessage(hit['title'], hit['url']))
+        return {"Hacker News" : messages}
