8119c48b1d9ec278f74f575a03e5c46dc7f84392
Connor Murray <connormurray7@gmail.com>
2016-10-12 21:16:17 -0500

    Parser interface

--- api_parser.py
+++ api_parser.py
@@ -7,6 +7,17 @@ class APIParser:
         self.interfaces = []
         self._set_interfaces()
 
+    def request(self, req):
+        if(req in self.cache):
+            return self.cache[req]
+        self.cache[req] = self._request(req)
+
+    def _request(self, req):
+        r = "["
+        for i in self.interfaces:
+            r += i.get_json(req) + ","
+        return r[:-1]
+
     def _set_interfaces(self):
         self.interfaces.append(StackOverFlow())
         self.interfaces.append(HackerNews())
