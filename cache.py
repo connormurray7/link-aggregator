9ed24a9f186660e0a183577b67129013b1e0c2cb
Connor Murray <connormurray7@gmail.com>
2016-10-30 12:34:04 -0500

    cache doucmentation

--- cache.py
+++ cache.py
@@ -1,3 +1,11 @@
+"""
+Created by Connor Murray (connormurray7@gmail.com)
+on 10/18/2016
+
+Front door to the data being displayed on the page.
+If the request is not cached then it passes along the search
+term to every external API interface to make a request.
+"""
 from collections import OrderedDict
 from web_interfaces import *
 from message import LinkAggMessage
@@ -5,6 +13,11 @@ import json
 
 
 class LinkAggCache:
+    """ Returns JSON for a given request.
+
+    Contains a list of interfaces that it will go to if it has not seen a request.
+    Every request is cached with an LRU cache of size LRU_CACHE_SIZE.
+    """
 
     LRU_CACHE_SIZE = 2048
 
@@ -14,6 +27,7 @@ class LinkAggCache:
         self._set_interfaces()
 
     def request(self, req):
+        """Accepts request and caches result if not seen before/recently."""
         if req in self.cache:
             return self.cache[req]
         self.cache[req] = self._request(req)
@@ -32,12 +46,22 @@ class LinkAggCache:
 
 
 class LRUCache(OrderedDict):
+    """Stores a JSON string of a request.
+
+    Inherits from OrderedDict so the least recently used item
+    can be evacuated efficiently.
+
+    Attributes:
+        capacity: the maximum size of the cache.
+        __setitem__: overrides the OrderedDict __setitem__
+    """
 
     def __init__(self, capacity):
         self.capacity = capacity
         super()
 
     def __setitem__(self, key, value):
+        """Will evacute the LRU item if cache is full."""
         if len(self) == self.capacity:
             self.popitem(False)
         OrderedDict.__setitem__(self, key, value)
