6b85c45f9e78ab654f97710a2628fd10bb47b4ff
Connor Murray <connormurray7@gmail.com>
2017-01-24 23:18:47 -0600

    redis support

--- linkagg/cache.py
+++ linkagg/cache.py
@@ -10,6 +10,7 @@ import logging
 import json
 import time
 import configparser
+import redis
 from collections import OrderedDict, deque
 from linkagg.message import LinkAggMessage
 from linkagg.web_interfaces import StackOverFlow, HackerNews, Github
@@ -23,28 +24,32 @@ class LinkAggCache(object):
     """
 
     def __init__(self, handler):
+        cfg = configparser.ConfigParser()
+        cfg.read('settings.ini')
+        cache_cfg = cfg['Cache']
+
+        self.interfaces = []
+        self._set_interfaces(cfg)
         self.requests = deque()
+        self.rate_limit = int(cfg['Cache']['rate.limit'])
+        self.cache = redis.StrictRedis(host=cache_cfg['host'], cache_cfg['port'])
 
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
         self.logger.addHandler(handler)
 
-        cfg = configparser.ConfigParser()
-        cfg.read('settings.ini')
-        self.interfaces = []
-        self._set_interfaces(cfg)
-        self.rate_limit = int(cfg['Cache']['rate.limit'])
-        self.cache = LRUCache(int(cfg['Cache']['size']))  # In memory caching layer.
 
     def request(self, req):
         """Accepts request and caches result if not seen before/recently."""
-        if req in self.cache:
-            return self.cache[req]
+        resp = self.cache.get(req)
+        if resp is not None:
+            return resp
         if self._need_rate_limit():
             self.logger.info("Rate limiting request: " + req)
             return "Request error"
-        self.cache[req] = self._request(req)
-        return self.cache[req]
+        resp = self._request(req)
+        self.cache.set(req, resp)
+        return resp
 
     def _request(self, req):
         self.logger.info(req + " was not cached, making API requests")
@@ -66,27 +71,3 @@ class LinkAggCache(object):
         self.requests.popleft()
         self.requests.append(time.time())
         return True
-
-
-class LRUCache(OrderedDict):
-    """Stores a JSON string of a request.
-
-    Inherits from OrderedDict so the least recently used item
-    can be evacuated efficiently.
-
-    Attributes:
-        capacity: the maximum size of the cache.
-        __setitem__: overrides the OrderedDict __setitem__
-    """
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.logger = logging.getLogger(__name__)
-        super().__init__()
-
-    def __setitem__(self, key, value):
-        """Will evacuate the LRU item if cache is full."""
-        if len(self) == self.capacity:
-            self.logger.info("Cache reached capacity " + self.capacity + " evacuating item")
-            self.popitem(False)
-        OrderedDict.__setitem__(self, key, value)
